224577885a923d7aabe95b23b3a1979c2bf52bba
jbjobclement <clement@jbjob.com.tw>
2024-01-17 13:16:22 +0800

    refactor: basic
    adjust:
    move content from for-demo to basic component

--- src/app/for-demo/basic/basic.component.ts
+++ src/app/for-demo/basic/basic.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NumberService } from 'src/app/shared/services/number.service';
 
 @Component({
   selector: 'app-basic',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./basic.component.scss']
 })
 export class BasicComponent {
-
+  numFromService: number;
+  constructor(
+    private numberService: NumberService
+  ) {
+    this.numFromService = this.numberService.numberFromService;
+  }
 }
--- src/app/for-demo/basic/basic.module.ts
+++ src/app/for-demo/basic/basic.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { BasicRoutingModule } from './basic-routing.module';
 import { BasicComponent } from './basic.component';
+import { HeaderModifierDirective } from 'src/app/shared/directives/header-modifier/header-modifier.directive';
+import { DirectiveWithServiceDirective } from 'src/app/shared/directives/directive-with-service/directive-with-service.directive';
 
 
 @NgModule({
@@ -11,7 +13,9 @@ import { BasicComponent } from './basic.component';
   ],
   imports: [
     CommonModule,
-    BasicRoutingModule
+    BasicRoutingModule,
+    HeaderModifierDirective,
+    DirectiveWithServiceDirective
   ]
 })
 export class BasicModule { }
