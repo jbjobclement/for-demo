2c717b42c9e87b77243f357b365ac4953bacbd60
jbjobclement <clement@jbjob.com.tw>
2024-02-17 15:50:40 +0800

    feat: for debugging demo
    reference: https://code.visualstudio.com/docs/nodejs/angular-tutorial#_debugging-angular

--- src/app/for-demo/js-array/js-array.component.ts
+++ src/app/for-demo/js-array/js-array.component.ts
@@ -6,20 +6,17 @@ import { Component } from '@angular/core';
 })
 export class JsArrayComponent {
   numbers = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10];
-
   newStructure = this.numbers.map(number => {
     return {
       number: number
     }
   })
-  
   numbersGreaterThanSix = this.numbers.filter(number => {
     if (number > 6) {
       return true;
     }
     return false;
   })
-
   uniqueNumbersArray: number[] = this.numbersGreaterThanSix.reduce((accumulator: number[], currentValue) => {
     if (accumulator.indexOf(currentValue) === -1) {
       return [
@@ -33,4 +30,12 @@ export class JsArrayComponent {
   sum = this.uniqueNumbersArray.reduce((accumulator, currentValue) => {
     return accumulator + currentValue
   }, 0)
+  constructor() {
+    console.log('this.numbers')
+    console.log(this.numbers)
+  }
+
+  planA () {
+    
+  }
 }
